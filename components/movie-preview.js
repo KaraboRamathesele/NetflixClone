d68a6b9874c1641b9d9df469fea543ece0999b93
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-10-10 13:02:43 +0200

    setting up movie component

--- components/movie-preview.js
+++ components/movie-preview.js
@@ -1,7 +1,55 @@
 
 class Component extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = '<div>THIS IS THE MOVIE PREVIEW</div>'
+    inner = this.attachShadow({ mode: 'closed '})
+
+    data = {
+        label: this.getAttribute('label'),
+        image: this.getAttribute('image'),
+
+    }
+
+    connectedCallback() { 
+        this.inner.innerHTML = /* html */ `
+            <style>
+               * { box-sizing: border-box } 
+
+               .resting {
+                    border: 1px solid grey;
+                    padding: 1rem;
+                    background: url("${this.data.image}");
+                    background-size: cover;
+                    background-position: center;
+                    cursor: pointer;
+                    position: relative;
+                    height: 200px;
+                    width: 400px;
+                    
+               }
+
+               .preview {
+                    display: none;
+                    position: absolute;
+                    top: -40%;
+                    left: -20%;
+                    width: 140%;
+                    height: 200%;
+                    background: black;
+               }
+
+               .resting:hover {
+                z-index: 10;
+               }
+
+               .resting:hover > .preview {
+                    display: block;
+               }
+            </style>
+
+            <div class='resting'>
+            <div class='preview'>${this.data.label}</div>
+            </div>
+            
+        `
     }
 }
 
